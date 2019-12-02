9c8050daa4dae06a0bb303fd84110a47bbba4224
nilesh-hegde <55364143+nilesh-hegde@users.noreply.github.com>
2019-12-03 02:46:14 +0530

    Update tone_analysis_api.py

--- tone_analysis_api.py
+++ tone_analysis_api.py
@@ -2,7 +2,7 @@ from watson_developer_cloud import ToneAnalyzerV3
 import json
 
 def cl_ana(text):
-    tone_analyzer = ToneAnalyzerV3(version='2017-09-21',username='51bd2236-b9c3-44a9-835e-d549aa6656d8',password='wHi1zHMZJYmN', url='https://gateway.watsonplatform.net/tone-analyzer/api')
+    tone_analyzer = ToneAnalyzerV3(version='********',username='****************',password='********************', url='https://gateway.watsonplatform.net/tone-analyzer/api')
     tone_analysis = tone_analyzer.tone({'text': text},'application/json').get_result()
     a1=tone_analysis['document_tone']
     a2=a1['tones']
